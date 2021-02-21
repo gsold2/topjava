3b90295e121f18f843450d5b2d2d5ebdf8c320e2
gsold2 <gsold2@gmail.com>
2021-02-21 20:24:24 +0300

    HW3

--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -1,3 +1,4 @@
+DROP TABLE IF EXISTS meals;
 DROP TABLE IF EXISTS user_roles;
 DROP TABLE IF EXISTS users;
 DROP SEQUENCE IF EXISTS global_seq;
@@ -23,3 +24,15 @@ CREATE TABLE user_roles
     CONSTRAINT user_roles_idx UNIQUE (user_id, role),
     FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
+
+CREATE TABLE meals
+(
+    id          SERIAL PRIMARY KEY,
+    user_id     INTEGER   NOT NULL,
+    dateTime    TIMESTAMP NOT NULL,
+    description VARCHAR   NOT NULL,
+    calories    INTEGER   NOT NULL,
+    CONSTRAINT meals_idx UNIQUE (user_id, dateTime),
+    FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+);
+CREATE INDEX meals_user_id_idx ON meals (user_id);
--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,3 +1,4 @@
+DELETE FROM meals;
 DELETE FROM user_roles;
 DELETE FROM users;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
@@ -9,3 +10,11 @@ VALUES ('User', 'user@yandex.ru', 'password'),
 INSERT INTO user_roles (role, user_id)
 VALUES ('USER', 100000),
        ('ADMIN', 100001);
+
+INSERT INTO meals (user_id, dateTime, description, calories)
+VALUES (100000, '2015-01-19 09:23:54', 'User завтрак', 510),
+       (100000, '2015-01-19 13:23:54', 'User обед', 1000),
+       (100000, '2015-01-19 10:23:54', 'User ужин', 1500),
+       (100001, '2015-01-19 09:33:54', 'ADMIN завтрак', 410),
+       (100001, '2015-01-19 13:33:54', 'ADMIN обед', 900),
+       (100001, '2015-01-19 10:33:54', 'ADMIN ужин', 100);
